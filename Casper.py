d327dd1f3ec655d95b39e7acb2972787ff8b0028
Jesse Rodarte <jerodarte@gmail.com>
2018-11-03 19:35:52 -0700

    he says hello
    and I'm very proud

--- Casper.py
+++ Casper.py
@@ -1,7 +1,9 @@
 import discord
+from discord.ext import commands
 import disc_config as cfg
 
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
@@ -11,14 +13,15 @@ async def on_ready():
 async def on_message(message):
     author = message.author
     msg = message.content
+    channel = message.channel
     print('message received from {}: {}'.format(author, msg))
 
     if message.author == client.user:
         print('oops')
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('hello'):
         print('yay')
-        await message.channel.send('Hello!')
+        await client.send_message(channel, 'hello!')
 
 client.run(cfg.disc_token)
