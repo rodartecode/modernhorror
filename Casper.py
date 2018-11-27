83bf7e22be950b7807ddf41e325b19032116cab9
JacobTheMiro <45219482+JacobTheMiro@users.noreply.github.com>
2018-11-26 19:29:01 -0800

    Update Casper.py

--- Casper.py
+++ Casper.py
@@ -10,13 +10,15 @@ bot = commands.Bot(command_prefix = "!")
 class Stats:
     option = 0
     temperment = 0
+    messageArray = ["Ok, no turning back now! \nSo, are you a man or a woman?",
+                    ".....A MANLY MAN!", "A LADAYYY!!!", "...a dog???", "Hey, it's you, Casper!" ]
 
 options = Stats()
+
 
 @client.event
 async def on_ready():
     print("The bot is ready!")
-
     await client.change_presence(game=discord.Game(name="Making a bot"))
 
 @client.event
@@ -26,6 +28,7 @@ async def on_message(message):
     if message.content == "Hello":
         await client.send_message(message.channel, "World")
     if message.content == "Start" or message.content == "start":
+        options.option = 0
         await start_loop(message)
     if message.content == "Die":
         exit()
@@ -35,69 +38,66 @@ async def on_message(message):
 
 @client.event
 async def start_loop(message):
-    if options.option == 0:
-        await client.send_message(message.channel, "Ok, no turning back now!")
-        await client.send_message(message.channel, "So, are you a man or a woman...")
-        funmsg = await client.send_message(message.channel, "????")
-        await client.add_reaction(funmsg, '👨')
-        await client.add_reaction(funmsg, '👩')
-        await client.add_reaction(funmsg, '🐕')
-        await client.add_reaction(funmsg, '👻')
+    emojispam = ['👨', '👩', '🐕', '👻']
 
-    elif options.option == 1:
-        await client.send_message(message.channel, ".....A MANLY MAN!")
-        options.option = 0
-    elif options.option == 2:
-        await client.send_message(message.channel, "A LADAYYY!!!")
-        options.option = 0
+    if options.option == 0:
+        await client.send_message(message.channel, "Ok, no turning back now! \nSo, are you a man or a woman?")
+        emb = (discord.Embed(description="👨: You choose a man \n👩: You choose a woman \n🐕: You choose a dog \n👻: You choose a ghost", colour = 0x3DF270))
+        funmsg = await client.send_message(message.channel, embed=emb)
+        
+        """this loop adds the reactions"""
+        for emoji in emojispam:
+            await client.add_reaction(funmsg, emoji)
+
     elif options.option == 3:
         options.option = 0
         await client.send_message(message.channel, ".....you like a cake?")
         unfunmsg = await client.send_message(message.channel, "????")
         await client.add_reaction(unfunmsg, '👍')
         await client.add_reaction(unfunmsg, '👎')
-    elif options.option == 6:
+    
+    else:
+        await client.send_message(message.channel, options.messageArray[options.option])
         options.option = 0
 
-
-
-
-
-
+    
     
 @client.event
 async def on_reaction_add(reaction, user):
-    emojispam = ['👨', '👩', '🐕']
+
     if reaction.emoji == '👨' and reaction.count > 1:
-         await client.send_message(reaction.message.channel, "A man!")
          options.option = 1
          await client.delete_message(reaction.message)
          await start_loop(reaction.message)
+
     if reaction.emoji == '👩' and reaction.count > 1:
-         await client.send_message(reaction.message.channel, "A woman!")
+
          options.option = 2
          await client.delete_message(reaction.message)
          await start_loop(reaction.message)
+
     if reaction.emoji == '🐕' and reaction.count > 1:
-         await client.send_message(reaction.message.channel, "A........ dog?")
          options.option = 3
          await client.delete_message(reaction.message)
          await start_loop(reaction.message)
-    if reaction.emoji == '👍' and reaction.count > 1:
-         await client.send_message(reaction.message.channel, "GOOOOOOOOO FUCK YOURSELLLLLLLLF!!!!!!!!!!")
+
+    if reaction.emoji == '👻' and reaction.count > 1:
          options.option = 4
          await client.delete_message(reaction.message)
          await start_loop(reaction.message)
-    if reaction.emoji == '👎' and reaction.count > 1:
-         await client.send_message(reaction.message.channel, "Oh I see. Have a nice day!")
+
+    if reaction.emoji == '👍' and reaction.count > 1:
+         await client.send_message(reaction.message.channel, "GOOOOOOOOO FUCK YOURSELLLLLLLLF!!!!!!!!!!")
          options.option = 5
          await client.delete_message(reaction.message)
          await start_loop(reaction.message)
-    if reaction.emoji == '👻' and reaction.count > 1:
-         await client.send_message(reaction.message.channel, "That's you, Casper!")
+
+    if reaction.emoji == '👎' and reaction.count > 1:
+         await client.send_message(reaction.message.channel, "Oh I see. Have a nice day!")
          options.option = 6
          await client.delete_message(reaction.message)
          await start_loop(reaction.message)
+
 
 
 
