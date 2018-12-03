27a8fc03d99b8ccdd3794b9a23a520057b16f96d
Jesse Rodarte <jerodarte@gmail.com>
2018-12-02 23:43:16 -0800

    small fixes

--- Util/embedTemplates.py
+++ Util/embedTemplates.py
@@ -17,12 +17,12 @@ async def on_ready():
 #Exposition Embed - embed template for scene-setting bits of text
 @client.command()  # ?? Will this be a command or an event?
 async def displayExpoEmbed ():
-    xml_title = #Get scene title from XML
-    xml_description = #Get scene description from XMl
+    xml_title = "" #Get scene title from XML
+    xml_description = "" #Get scene description from XMl
     expoEmbed = discord.Embed(
         title = xml_title,
         description = xml_description,
-        color = discord.Color.black()
+        color = discord.Color.default()
     )
 
     expoEmbed.set_author(name = '') # ?? Should this be set to the MasterBot's name? The level/room name? Or left blank?
@@ -32,7 +32,7 @@ async def displayExpoEmbed ():
 #(where the bot will display the different actions a user can take, and the corresponding emojis)
 @client.command() # ?? Will this be a command or an event?
 async def displayPromptEmbed ():
-    xml_title = #Get scene title from XML
+    xml_title = "" #Get scene title from XML
     promptEmbed = discord.Embed(
         title = xml_title,
         description = 'Choose what action you will take', #Not sold on this wording, open to suggestions
@@ -49,8 +49,8 @@ async def displayPromptEmbed ():
 #Response Embed - embed template for the bot's response to the user's actions
 @client.command() # ?? Will this be a command or an event?
 async def displayResponseEmbed ():
-    xml_title = #Get scene title from XML
-    xml_description = #Pull response from XMl based on corresponding action selected by user in the promptEmbed
+    xml_title = "" #Get scene title from XML
+    xml_description = "" #Pull response from XMl based on corresponding action selected by user in the promptEmbed
     responseEmbed = discord.Embed(
         title = xml_title, # Scene title pulled from XML
         description = xml_description,
