d333e94e2a34a82fd0a3b07535734b0701c3fc74
JacobTheMiro <45219482+JacobTheMiro@users.noreply.github.com>
2018-12-10 09:12:21 -0800

    Update MessageManager.py

--- MessageManager.py
+++ MessageManager.py
@@ -2,19 +2,9 @@ import discord
 from discord.ext import commands
 import asyncio
 
-
 client = discord.Client()
 
-
-class Stats:
-    roomNames = ["aether", "attic", "hallway", "masterbedroom", "nursery", "bathroom", "hallway", "kitchen", "livingroom", "diningroom", "study", "trophyroom"]
-
-
-options = Stats()
-
-
-#A very basic queue class. It is reversed in order (0 is the back, not the front)
-
+#updated with a clear function.
 class Queue:
     def __init__(self):
         self.items = []
@@ -24,25 +14,35 @@ class Queue:
         return self.items.pop()
     async def size(self):
         return len(self.items)
+    async def clear(self):
+        while await self.size() > 0:
+            self.items.pop()
 
-#Message Manager stores an array of 12 queues (one for each room). Once instantiated, the member function .enqueue(message) can be called
-#in on_message as messagemanager.enqueue(message) to automatically put the message into the right queue. the roomNames
-#array is used to determine the index of the queue array. The enqueue function ignores embeds.
 
 class MessageManager:
     def __init__(self):
+        self.roomNames = ["aether", "attic", "hallway", "masterbedroom", "nursery", "bathroom", "hall", "kitchen", "livingroom", "diningroom", "study", "trophyroom"]
         self.messageQueue = [Queue() for i in range(12)]
   
-    async def enqueue(self, message):
+
+    async def enqueue(self, message, server):
+        #this if statement allows MessageManager to ignore embeds, regardless of content.
         if message.content == "":
-            return
-        else:
-            if await self.messageQueue[options.roomNames.index(message.channel.name)].size() < 9:
-                print("enqueueing: " + message.content + " in channel " + message.channel.name)
-                await self.messageQueue[options.roomNames.index(message.channel.name)].enqueue(message)
-            else:
-                msg = await self.messageQueue[options.roomNames.index(message.channel.name)].dequeue()
-                print("dequeueing: " + msg.content + " in channel " + msg.channel.name)
-                await client.delete_message(msg)
-                await self.messageQueue[options.roomNames.index(message.channel.name)].enqueue(message)
-                print("enqueueing: " + message.content + " in channel " + message.channel.name)
+                return
+        for names in self.roomNames:
+            if message.channel.name == names:
+                if message.server == server:
+                    if await self.messageQueue[self.roomNames.index(message.channel.name)].size() < 9:
+                        print("enqueueing: " + message.content + " in channel " + message.channel.name)
+                        await self.messageQueue[self.roomNames.index(message.channel.name)].enqueue(message)
+                        return
+                    else:
+                        msg = await self.messageQueue[self.roomNames.index(message.channel.name)].dequeue()
+                        print("dequeueing: " + msg.content + " in channel " + msg.channel.name)
+                        await client.delete_message(msg)
+                        await self.messageQueue[self.roomNames.index(message.channel.name)].enqueue(message)
+                        print("enqueueing: " + message.content + " in channel " + message.channel.name)
+                        return
+
+
+
